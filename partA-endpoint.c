6c2c849f99d4cece468712167f2f1de86b9e7bd5
mosaicthej <mij623@usask.ca>
2024-03-07 16:37:31 -0600

    send-thread prototype

--- partA-endpoint.c
+++ partA-endpoint.c
@@ -66,3 +66,37 @@ int check_args(char *st_host, char *st_port, char *rf_port) {
   }
 }
 
+int send_thread(char *send_to_host, char *send_to_port) {
+  /* create a socket */
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    perror("socket");
+    return EXIT_FAILURE;
+  }
+
+  /* set up the address to send to */
+  struct addrinfo hints, *res;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  int status = getaddrinfo(send_to_host, send_to_port, &hints, &res);
+  if (status != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    return EXIT_FAILURE;
+  }
+
+  /* send data */
+  char *msg = "Hello, world!";
+  int numbytes =
+      sendto(sockfd, msg, strlen(msg), 0, res->ai_addr, res->ai_addrlen);
+  if (numbytes < 0) {
+    perror("sendto");
+    return EXIT_FAILURE;
+  }
+
+  freeaddrinfo(res);
+  close(sockfd);
+  return EXIT_SUCCESS;
+}
+
+
