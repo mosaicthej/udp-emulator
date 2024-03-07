03e2b74364a4ab5e243f5572eb7666a6305c3c16
mosaicthej <mij623@usask.ca>
2024-03-07 16:28:34 -0600

    error handles

--- partA-endpoint.c
+++ partA-endpoint.c
@@ -13,3 +13,34 @@
 
 #include <pthread.h>
 
+#define handle_error_en(en, msg)                                               \
+  do {                                                                         \
+    errno = en;                                                                \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+#define handle_error(msg)                                                      \
+  do {                                                                         \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+/* validate port numbers */
+int check_args(char* st_host, char* st_port, char* rf_port){
+  int st_p = atoi(st_port);
+  int rf_p = atoi(rf_port);
+  if(st_p < PORTMIN || st_p > PORTMAX){
+    fprintf(stderr, "send-to-port must be between %d and %d\n", 
+            PORTMIN, PORTMAX);
+    return EXIT_FAILURE;
+  }
+  if(rf_p < PORTMIN || rf_p > PORTMAX){
+    fprintf(stderr, "receive-from-port must be between %d and %d\n", 
+            PORTMIN, PORTMAX);
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+  }
+}
+
