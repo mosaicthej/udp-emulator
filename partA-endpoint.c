b83b6c7ea4bb3114722d1c7316a1e7af2e42a540
mosaicthej <mij623@usask.ca>
2024-03-07 19:54:20 -0600

    restored the send_thread from vim crash (kindsof)

--- partA-endpoint.c
+++ partA-endpoint.c
@@ -166,6 +166,33 @@ int main(int argc, char *argv[]) {
 }
 
 int send_thread(char *send_to_host, char *send_to_port) {
+
+  /* send data */
+  char *msg = "Hello, world!";
+  int numbytes =
+      sendto(sockfd, msg, strlen(msg), 0, res->ai_addr, res->ai_addrlen);
+  if (numbytes < 0) {
+    perror("sendto");
+    return EXIT_FAILURE;
+  }
+  /* filling the information for the sender and receiver threads */
+  send_info.send_to_host = send_to_host;
+  send_info.send_to_port = send_to_port;
+  recv_info.receive_from_port = receive_from_port;
+
+
+  /* set up the address to send to */
+  struct addrinfo hints, *res;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  int status = getaddrinfo(send_to_host, send_to_port, &hints, &res);
+  if (status != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    return EXIT_FAILURE;
+  }
+
+
   /* create a socket */
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sockfd < 0) {
